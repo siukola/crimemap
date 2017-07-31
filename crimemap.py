564ff418c808d0e05663a6a48a8d3728fae51e40
siukola <siukola@example.com>
2017-07-31 10:59:08 +0300

    Input validation.

--- crimemap.py
+++ crimemap.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import json
+import datetime
+import dateparser
+import string
 import dbconfig
 if dbconfig.test:
     from mockdbhelper import MockDBHelper as DBHelper
@@ -11,23 +14,36 @@ else:
 app = Flask(__name__)
 DB = DBHelper()
 
+categories = ['mugging', 'break-in']
+
 @app.route("/")
-def home():
+def home(error_message=None):
     try:
         crimes = DB.get_all_crimes()
         crimes = json.dumps(crimes)
     except Exception as e:
         print e
         crimes = None
-    return render_template("home.html", crimes=crimes)
+    return render_template("home.html", crimes=crimes, categories=categories, error_message=error_message)
 
 @app.route("/submitcrime", methods=['POST'])
 def submitcrime():
     category = request.form.get("category")
+    if category not in categories:
+        return home()
     date = request.form.get("date")
-    latitude = float(request.form.get("latitude"))
-    longitude = float(request.form.get("longitude"))
-    description = request.form.get("description")
+
+    try:
+        latitude = float(request.form.get("latitude"))
+        longitude = float(request.form.get("longitude"))
+    except ValueError:
+        return home()
+
+    date = format_date(request.form.get("date"))
+    if not date:
+        return home("Invalid date. Please use yyyy-mm-dd format")
+    
+    description = sanitize_string(request.form.get("description"))
     DB.add_crime(category, date, latitude, longitude, description)
     return home()
 
@@ -47,6 +63,17 @@ def clear():
     except Exception as e:
         print e
     return home()
+
+def format_date(userdate):
+    date = dateparser.parse(userdate)
+    try:
+        return datetime.datetime.strftime(date, "%Y-%m-%d")
+    except TypeError:
+        return None
+
+def sanitize_string(userinput):
+    whitelist = string.letters + string.digits + " !?$.,;:-'()&"
+    return filter(lambda x: x in whitelist, userinput)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
